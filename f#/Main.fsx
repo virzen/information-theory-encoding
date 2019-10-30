b898f412c7a074ecc56126106c5e76ee3714a72f
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2019-10-31 00:44:13 +0100

    rewrite

--- f#/Main.fsx
+++ f#/Main.fsx
@@ -4,60 +4,89 @@ open System
 let inputFilename = "116-binary-tree.pdf"
 let binaryFilename = "binary.01"
 
+
+// bytes
+
+let readFileAsBytes filename =
+  File.ReadAllBytes(filename)
+
 let intToBinary (x: int) =
   Convert.ToString(x, 2)
 
 let padWithZeros length (x: string) =
   x.PadLeft(length, '0')
 
-let byteToBinary =
-  int >> intToBinary >> (padWithZeros 8)
+let byteToBinaryOfWordLength wordLength =
+  int >> intToBinary >> (padWithZeros wordLength)
 
-let parseFileToBinary filename =
-  filename
-    |> File.ReadAllBytes
-    |> Array.map byteToBinary
+let bytesToBinaryOfWordLength wordLength bytes =
+  bytes
+    |> Array.map (byteToBinaryOfWordLength wordLength)
     |> String.concat ""
 
-let writeToFile filename s =
-  File.WriteAllText(filename, s)
+
+// binary
+
+let readFileAsBinary filename =
+  File.ReadAllText filename
 
 let binaryToInt b =
   Convert.ToInt32(b, 2)
 
-let parseBinaryFileToInts binaryLength filename =
-  filename
-  |> File.ReadAllText
-  |> fun s -> s.ToCharArray()
-  |> Array.chunkBySize binaryLength
-  |> Array.map System.String
-  |> Array.map binaryToInt
+let binaryOfWordLengthToInts wordLength (s: string) = // custom format instead of string?
+  s.ToCharArray()
+    |> Array.chunkBySize wordLength
+    |> Array.map System.String
+    |> Array.map binaryToInt
 
-type ByteOccurences = Map<int, int>
+let writeToFile filename s =
+  File.WriteAllText(filename, s)
 
-let incrementCountOfByte map (byte: int) =
-  let maybePreviousOccurences = Map.tryFind byte map
+
+// distribution
+
+type Distribution = Map<int, int>
+
+let incrementCountOf map (value: int) =
+  let maybePreviousOccurences = Map.tryFind value map
   
   let newOccurences = 
     match maybePreviousOccurences with
       | Some x -> x + 1
       | None -> 1
 
-  Map.add byte newOccurences map
+  Map.add value newOccurences map
 
+let distribution (values: int[]): Distribution =
+    Array.fold incrementCountOf Map.empty values
 
-let countOccurences (bytes: int[]): ByteOccurences =
-    Array.fold incrementCountOfByte Map.empty bytes
 
 // MAIN
 
 inputFilename
-  |> parseFileToBinary
+  |> readFileAsBytes
+  |> bytesToBinaryOfWordLength 8
   |> writeToFile binaryFilename
 
 binaryFilename
-  |> parseBinaryFileToInts 8
-  |> countOccurences
+  |> readFileAsBinary
+  |> binaryOfWordLengthToInts 8
   |> printf "%A"
 
-// TODO: accumulate results in a map
+binaryFilename
+  |> readFileAsBinary
+  |> binaryOfWordLengthToInts 8
+  |> distribution
+  |> printf "%A"
+
+binaryFilename
+  |> readFileAsBinary
+  |> binaryOfWordLengthToInts 4
+  |> distribution
+  |> printf "%A"
+
+binaryFilename
+  |> readFileAsBinary
+  |> binaryOfWordLengthToInts 1
+  |> distribution
+  |> printf "%A"
